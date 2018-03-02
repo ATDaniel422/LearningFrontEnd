96cf96bf963a127457e54ecd8ef107972e0c10ab
ATDaniel422 <ATDaniel422@gmail.com>
2018-03-01 21:51:37 -0500

    Add files via upload

---
+++ Web Development (HTML, CSS, Javascript)/JavaScript/functions.js
@@ -0,0 +1,10 @@
+//Function Definition
+
+function displayGreeting() {
+    document.write("Hello, fellow human.");
+}
+
+function displayPlayerScore(theScore) {
+    
+    document.write("<br/>Player Score: " + theScore);
+}
